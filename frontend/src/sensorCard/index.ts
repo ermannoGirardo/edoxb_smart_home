183e20e32774e961ae216dad9797922f39abfc97
developer <developer@ocean.cloud>
2025-12-13 03:44:26 +0100

    Rimuove sensorCard dal repository (generata automaticamente durante il build)

--- frontend/src/sensorCard/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ShellyRGBW2Control } from './ShellyRGBW2Control'
-export { default as ShellyDimmer2Control } from './ShellyDimmer2Control'
-export { default as ArduinoWebSocketControl } from './ArduinoWebSocketControl'
